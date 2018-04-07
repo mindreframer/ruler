706b6e1f25e6f44d6a9fa4f7509fb3036a533ce6
Roman Heinrich <roman.heinrich@gmail.com>
2018-04-07 23:46:22 +0200

    really basic stuff here

--- lib/interpreter.ex
+++ lib/interpreter.ex
@@ -11,6 +11,14 @@ defmodule Ruler.Interpreter do
     reduce(op1) - reduce(op2)
   end
 
+  def reduce("/", op1, op2) do
+    reduce(op1) / reduce(op2)
+  end
+
+  def reduce("*", op1, op2) do
+    reduce(op1) * reduce(op2)
+  end
+
   def reduce(expr) when is_number(expr) do
     expr
   end
--- test/interpreter_test.exs
+++ test/interpreter_test.exs
@@ -5,15 +5,14 @@ defmodule Ruler.InterpreterTest do
 
 
   def check(expr, out) do
-    assert apply(Ruler.Interpreter.reduce, expr) == out
+    assert apply(Ruler.Interpreter, :reduce, expr) == out
   end
 
   test "greets the world" do
-    assert Ruler.Interpreter.reduce(
-             "+",
-             1,
-             3
-           ) == 4
+    check(["+", 1, 2], 3)
+    check(["-", 5, 4], 1)
+    check(["/", 5.0, 2], 2.5)
+    check(["*", 6, 2], 12)
   end
 end
 
