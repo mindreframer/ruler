ced2d3daed4e54c96879cdb84ea2afc16036a5d6
Roman Heinrich <roman.heinrich@gmail.com>
2018-04-08 02:42:23 +0200

    tests for kv_agent

--- test/kv_agent_test.exs
+++ test/kv_agent_test.exs
@@ -10,4 +10,15 @@ defmodule Ruler.KVAgentTest do
     Ruler.KVAgent.reset(ctx)
     assert Ruler.KVAgent.get(ctx, ["a"]) == nil
   end
+
+  test "nested paths require existence" do
+    {:ok, ctx} = Ruler.KVAgent.start_link([])
+    assert Ruler.KVAgent.get(ctx, ["a", "b"]) == nil
+    assert Ruler.KVAgent.get(ctx, ["a", "b", "c"]) == nil
+    assert Ruler.KVAgent.set(ctx, ["a", "b"], 1) == {:error, :parent_path_does_not_exist}
+    assert Ruler.KVAgent.get(ctx, ["a", "b"]) == nil
+    assert Ruler.KVAgent.set(ctx, ["a"], %{}) == :ok
+    assert Ruler.KVAgent.set(ctx, ["a", "b"], 1) == :ok
+    assert Ruler.KVAgent.get(ctx, ["a", "b"]) == 1
+  end
 end
