d20eb1ab612cadbadd401ace5423a4b8c4dd9050
Roman Heinrich <roman.heinrich@gmail.com>
2018-04-22 22:50:54 +0200

    benchmarker

--- lib/benchmarker.ex
+++ lib/benchmarker.ex
@@ -1,8 +1,9 @@
 defmodule Ruler.Benchmarker do
   def basic_ops do
     Benchee.run(%{
-      "plain"    => fn -> Ruler.InterpreterList.reduce(%{}, ["+", ["-", 5, 4], 2]) end,
-      "from env" => fn -> Ruler.InterpreterList.reduce(%{"a" => %{"b" => 5}}, ["+", ["-", [".", "a", "b"], 4], 2]) end,
+      # "plain"    => fn -> Ruler.InterpreterList.reduce(%{}, ["+", ["-", 5, 4], 2]) end,
+      # "from env" => fn -> Ruler.InterpreterList.reduce(%{"a" => %{"b" => 5}}, ["+", ["-", [".", "a", "b"], 4], 2]) end,
+      "stateless" => fn -> Ruler.InterpreterStateless.eval_ast(%{"bindings" => %{"a" => %{"b" => 5}}}, ["+", ["-", [".", "a", "b"], 4], 2]) end,
     },  time: 5, formatter_options: %{console: %{extended_statistics: true}})
 
   end
